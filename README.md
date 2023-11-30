a889f443f33b5725b6615db6e07256afd38a7d99
nardor <nard0r@protonmail.com>
2023-11-30 20:55:52 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Ignore-aggregator
+
+## Foreword
+
+My issue is that I couldn't figure out how to have `tar` ignore `.gitignore` pointed files in subdirectories.
+
+This project consists in 30 lines of rust to solve this very specific problem.
+
+There is most certainly some more efficient way to use `tar` and to ignore unwanted files indicated by `.gitignore` files.
+
+However after trying a bit I couldn't succeed.
+I wasn't satisfied with neither `--exclude-ignore=".gitignore"` nor `--exclude-vcs-ignores`.
+
+## What it does
+
+I wanted a way to aggregate all `.gitignore`(s) in a recursive way under some directory.
+
+It allows me to use the generated file as an input for my backup `tar` command.
+
+```bash
+# the directory I would like to backup
+❯ tree DIR_TO_BACKUP
+├── project1
+│   ├── build
+│   └── .gitignore
+└── subdir
+    ├── project2
+    │   ├── .gitignore
+    │   └── target
+    └── project3
+        ├── .gitignore
+        └── node_modules
+
+❯ bat DIR_TO_BACKUP/❯ bat **/.gitignore
+───────┬────────────────────────────────────────────────
+       │ File: project1/.gitignore
+───────┼────────────────────────────────────────────────
+   1   │ /build
+───────┴────────────────────────────────────────────────
+───────┬────────────────────────────────────────────────
+       │ File: subdir/project2/.gitignore
+───────┼────────────────────────────────────────────────
+   1   │ target
+───────┴────────────────────────────────────────────────
+───────┬────────────────────────────────────────────────
+       │ File: subdir/project3/.gitignore
+───────┼────────────────────────────────────────────────
+   1   │ node_modules/*
+───────┴────────────────────────────────────────────────
+
+❯ cd DIR_TO_BACKUP
+
+❯ ignore-aggregator -r . -o TO_IGNORE
+Scanning for .gitingore files
+Found 3 git ignore files
+"./subdir/project3/.gitignore"
+"./subdir/project2/.gitignore"
+"./project1/.gitignore"
+
+❯ bat TO_IGNORE
+───────┬────────────────────────────────────────────────
+       │ File: TO_IGNORE
+───────┼────────────────────────────────────────────────
+   1   │ ./subdir/project3/node_modules/*
+   2   │ ./subdir/project2/target
+   3   │ ./project1/build
+───────┴────────────────────────────────────────────────
+
+# now I can feed this to my `tar` command
+❯ tar --exclude-from=TO_IGNORE -cvaf backup.tar.gz  
+```
---
+++ src/main.rs
@@ -0,0 +1,139 @@
+use std::{
+    fs::{read_dir, File},
+    io::{Read, Result as IOResult, Write},
+    path::Path,
+};
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct IgnoreAggregatorArgs {
+    #[arg(short, long)]
+    reference_directory: String,
+
+    #[arg(short, long)]
+    output_aggregated: String,
+}
+
+fn check_args(args: &IgnoreAggregatorArgs) -> Result<(), String> {
+    let reference_path = Path::new(&args.reference_directory);
+
+    if !(reference_path.exists() && reference_path.is_dir()) {
+        return Err(format!(
+            "Invalid reference directory {}",
+            args.reference_directory
+        ));
+    }
+
+    let output_file = Path::new(&args.output_aggregated);
+    if output_file.exists() {
+        return Err(format!(
+            "Invalid output file. {:?} already exists.",
+            output_file
+        ));
+    }
+    if let Some(output_parent) = output_file.parent() {
+        if output_parent.to_str().unwrap() == "" {
+            () // it's OK, don't check for existence
+        } else if !(output_parent.exists()) {
+            return Err(format!(
+                "Invalid output path, couldn't find directory to create output file. ({:?} not found)",
+                output_parent
+            ));
+        }
+    }
+    // else we are at root maybe
+    Ok(())
+}
+
+fn list_git_ignore_files(reference_directory: &str) -> IOResult<Vec<String>> {
+    let reference_path = Path::new(reference_directory);
+    let mut to_explore = vec![reference_path.to_path_buf()];
+    let mut git_ignore_file_pathes = vec![];
+
+    while let Some(path) = to_explore.pop() {
+        let Ok(read_dir) = read_dir(path.clone()) else {
+            println!("[Error] Failed reading {:?}", &path);
+            continue;
+        };
+        for entry in read_dir {
+            if let Ok(entry) = entry {
+                let entry_path = entry.path();
+                if entry_path.is_dir() {
+                    to_explore.push(entry_path);
+                } else if entry_path.is_file()
+                    && entry_path.file_name().expect(
+                        "Expected to be able to extract filename while iterating in ReadDir",
+                    ) == ".gitignore"
+                {
+                    // unwrap because the former condition already has an "expect" on `entry_path.file_name()`
+                    git_ignore_file_pathes.push(entry_path.to_str().unwrap().to_owned());
+                }
+            }
+        }
+    }
+
+    Ok(git_ignore_file_pathes)
+}
+
+fn re_reference_git_ignore_file(git_ignore_path: &str) -> IOResult<Vec<String>> {
+    let git_ignore_path = Path::new(git_ignore_path);
+
+    let git_ignore_parent = git_ignore_path.parent().unwrap();
+
+    let mut git_file = File::open(git_ignore_path)?;
+    let mut git_ignore_str = String::new();
+    git_file.read_to_string(&mut git_ignore_str)?;
+
+    let re_referenced_git_ignore_lines: Vec<String> = git_ignore_str
+        .lines()
+        .filter(|&line| !line.starts_with('#'))
+        .map(|ignore| ignore.strip_prefix('/').unwrap_or(ignore))
+        .map(|ignore| git_ignore_parent.join(Path::new(&ignore)))
+        .map(|path| path.to_str().unwrap().to_owned())
+        .collect();
+
+    Ok(re_referenced_git_ignore_lines)
+}
+
+fn main() {
+    let args = IgnoreAggregatorArgs::parse();
+    match check_args(&args) {
+        Ok(()) => (),
+        Err(msg) => {
+            println!("[Error] {msg}");
+            return;
+        }
+    }
+    println!("Scanning for .gitignore files");
+    match list_git_ignore_files(&args.reference_directory) {
+        Ok(git_ignore_file_pathes) => {
+            let mut output_file = match File::options()
+                .create_new(true)
+                .write(true)
+                .open(args.output_aggregated)
+            {
+                Ok(file) => file,
+                Err(err) => {
+                    println!("[Error] {err:?}");
+                    return;
+                }
+            };
+            println!("Found {} git ignore files", git_ignore_file_pathes.len());
+            for path in git_ignore_file_pathes {
+                println!("{path:?}");
+                let re_referenced = re_reference_git_ignore_file(&path).unwrap();
+                for ignore in re_referenced {
+                    output_file.write_all(ignore.as_bytes()).unwrap();
+                    output_file.write(b"\n").unwrap();
+                }
+            }
+        }
+        Err(err) => {
+            println!("[Error] {err:?}");
+            return;
+        }
+    }
+
+    // println!("Args: {args:?}");
+}
